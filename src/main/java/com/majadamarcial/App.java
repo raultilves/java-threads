ae370e2f3ebf1b334f3224a9c542e621b0209f8d
raultilves <raulsantanatilves710@gmail.com>
2021-12-02 15:05:43 +0000

    Corredor B espera a A mediante await

--- src/main/java/com/majadamarcial/App.java
+++ src/main/java/com/majadamarcial/App.java
@@ -1,13 +1,108 @@
 package com.majadamarcial;
 
+import java.io.IOException;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    private static Lock candado = new ReentrantLock();
+    private static Condition relevo = candado.newCondition();
+    private static int turn = 1;
+
+    public static void main(String[] args) {
+        final Corredor corredor1 = new Corredor("A");
+        final Corredor corredor2 = new Corredor("B");
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    candado.lock();
+                    corredor1.avanzar();
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+                corredor1.retroceder();
+                turn = 2;
+                relevo.signalAll();
+                candado.unlock();
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    candado.lock();
+                    while (turn != 2) {
+                        relevo.await();
+                    }
+                    corredor2.avanzar();
+                } catch (InterruptedException | IOException e) {
+                    e.printStackTrace();
+                }
+                corredor2.retroceder();
+                candado.unlock();
+            }
+        });
+
+        t1.start();
+        t2.start();
+    }
+
+    public static final void clearConsole() throws InterruptedException, IOException {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();  
+    }
+}
+
+class Corredor {
+    private int position;
+    private String s;
+    private String nombre;
+
+    public Corredor(String nombre) {
+        this.position = 0;
+        this.s = "";
+        this.nombre = nombre;
+    }
+
+    public synchronized void avanzar() throws InterruptedException, IOException {        
+        App.clearConsole();
+       
+        System.out.print("Corredor " + this.nombre + ": ");
+
+        while (this.position < 10) {
+            try {
+                wait(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }  
+            System.out.print("#");
+            this.position++;
+        }
+    }
+
+    public synchronized void retroceder() {
+        while (this.position >= 0) {
+            try {
+                wait(500);
+                App.clearConsole();
+                this.s = "";
+            } catch (InterruptedException | IOException e1) {
+                e1.printStackTrace();
+            }
+            System.out.print("Corredor " + this.nombre + ": ");
+            for (int i = 0; i < position; i++) {
+                this.s += "#"; 
+            }
+            System.out.print(this.s);
+            this.position--;
+        }
     }
 }
